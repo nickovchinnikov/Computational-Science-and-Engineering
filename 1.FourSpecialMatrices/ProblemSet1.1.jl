43f3bb71554ca2774bd5b686dbf923acf4df472f
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2021-01-14 10:59:36 +0300

    Fix subscripts

--- 1.FourSpecialMatrices/ProblemSet1.1.jl
+++ 1.FourSpecialMatrices/ProblemSet1.1.jl
@@ -9,37 +9,37 @@ include("./KTBC.jl")
 import .KTBC.CreateKTBC
 
 # 3
-U_5 = I(5) - diagm(1 => ones(4))
-S_5 = triu(ones(5, 5))
+U₅ = I(5) - diagm(1 => ones(4))
+S₅ = triu(ones(5, 5))
 
-U_5 * S_5
+U₅ * S₅
 
-@test S_5 == inv(U_5)
+@test S₅ == inv(U₅)
 
 # 4
-S_4 = triu(ones(4, 4))
+S₄ = triu(ones(4, 4))
 
-S_4 * S_4'
+S₄ * S₄'
 
 # 5
-KTBC_2 = CreateKTBC()
-K_2 = convert(Array{Int}, KTBC_2[1])
+KTBC₂ = CreateKTBC()
+K₂ = convert(Array{Int}, KTBC₂[1])
 
-det(K_2)
-inv(K_2)
+det(K₂)
+inv(K₂)
 
-@test inv(K_2) * det(K_2) == [
+@test inv(K₂) * det(K₂) == [
   2.0 1.0
   1.0 2.0
 ]
 
-KTBC_3 = CreateKTBC(3)
-K_3 = convert(Array{Int}, KTBC_3[1])
+KTBC₃ = CreateKTBC(3)
+K₃ = convert(Array{Int}, KTBC₃[1])
 
-det(K_3)
-inv(K_3)
+det(K₃)
+inv(K₃)
 
-@test round.(Int, inv(K_3) * det(K_3)) == [
+@test round.(Int, inv(K₃) * det(K₃)) == [
   3 2 1
   2 4 2
   1 2 3
