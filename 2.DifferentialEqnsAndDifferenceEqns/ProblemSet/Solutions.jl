80a130e1d61a23a6cd489d0f252c70504c30a257
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2021-01-22 11:27:46 +0300

    Add solution for #13

--- 2.DifferentialEqnsAndDifferenceEqns/ProblemSet/Solutions.jl
+++ 2.DifferentialEqnsAndDifferenceEqns/ProblemSet/Solutions.jl
@@ -1,4 +1,5 @@
 using Test
+using Plots
 
 include("../../lib/DifferencesMatrix.jl")
 using .DifferencesMatrix: backwardDiff, forwardDiff, centeredDiff
@@ -13,3 +14,24 @@ backwardDiff(dim) * centeredDiff(dim)
 
 # 10
 forwardDiff(dim) * backwardDiff(dim)
+
+# 13
+function discretSolution(n::Int)
+    h = 1 / (n + 1)
+    T = backwardDiff(n) * (-forwardDiff(n))
+    u = h^2 * inv(T) * ones(n)
+    return u
+end
+
+trueSolutions(x) = (1 / 2) * (1 - x^2)
+
+steps = 0:1/8:1
+
+uₜ = trueSolutions.([steps...])[2:8]
+plot(uₜ)
+
+u₇ = discretSolution(7)
+plot!(u₇)
+
+e = uₜ - u₇
+plot(e)
