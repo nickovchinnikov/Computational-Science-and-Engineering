f61cbc379be236c44a78f22f76dd38006e51b83b
Nick Ovchinnikov <nickovchinnikof@gmail.com>
2021-01-28 18:04:34 +0300

    Add 9, 10, 11, 12

--- 3.SolvingALinearSystem/Solutions.jl
+++ 3.SolvingALinearSystem/Solutions.jl
@@ -4,7 +4,7 @@ using Plots
 include("../lib/KTBC.jl")
 using .KTBC: CreateKTBC
 
-using LinearAlgebra: I, SymTridiagonal, lu, det, Diagonal, diagm
+using LinearAlgebra: I, SymTridiagonal, lu, cholesky, det, eigen, Diagonal, diag, diagm, PosDefException
 
 # 1
 K₄ = CreateKTBC(4)[1]
@@ -54,8 +54,64 @@ end
 @test round(invL[4, 2], digits=1) == 1 / 2
 
 # 4
-d = (2:5)./(1:4)
+d = (2:5) ./ (1:4)
 
-L = I(4) - diagm(-1 => (1:3)./(2:4))
+L = I(4) - diagm(-1 => (1:3) ./ (2:4))
 
 L * Diagonal(d) * L'
+
+# 9
+K₃, T₃, B₃, C₃ = CreateKTBC(3)
+
+A = cholesky(convert(Array{Float64}, K₃))
+@test round.(A.L * A.U) == K₃
+
+A = cholesky(convert(Array{Float64}, T₃))
+@test round.(A.L * A.U) == T₃
+
+@test_throws PosDefException cholesky(convert(Array{Float64}, B₃))
+
+Bₑ = B₃ + eps() * I(3)
+A = cholesky(convert(Array{Float64}, Bₑ))
+@test round.(A.L * A.U) == round.(Bₑ)
+
+# 10
+eye₄ = ones(4, 4)
+eigen(eye₄)
+
+@test det(eye₄) == 0.0
+
+L = [
+  1
+  1
+  1
+  1
+]
+
+@test L * L' == eye₄
+
+# 11
+K = ones(4, 4) + I(4) / 100
+L, U = lu(K)
+eigen(K)
+inv(K)
+
+# 12 
+# https://rosettacode.org/wiki/Pascal_matrix_generation#Julia
+function pascal(n::Int)
+    return [binomial(j + i, i) for i in 0:n, j in 0:n]
+end
+
+# https://en.wikipedia.org/wiki/Pascal_matrix
+function pascalL(n::Int)
+    return exp(diagm(-1 => 1:(n - 1)))
+end
+
+K = pascal(4)
+L, U = lu(K)
+
+@test det(K) == abs(reduce(*, diag(U)))
+
+L = pascalL(5)
+
+@test K == round.(L * L')
